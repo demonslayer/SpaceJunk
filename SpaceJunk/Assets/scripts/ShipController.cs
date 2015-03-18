92a7aed017a4e1d8c2e407979dea6fc2141b220d
demonslayer <margaret.k.mcnulty@gmail.com>
2015-03-18 00:37:18 -0700

    can move around screen

---
+++ SpaceJunk/Assets/scripts/ShipController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShipController : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate() {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+		
+		Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+
+		rigidBody.velocity = movement * speed;
+
+	}
+}
