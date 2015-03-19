9528066bad0c70b016fd707edce05b52d4238bce
demonslayer <margaret.k.mcnulty@gmail.com>
2015-03-19 11:26:07 -0700

    you spin me right round baby right round

---
+++ SpaceJunk/Assets/scripts/Tumbler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tumbler : MonoBehaviour {
+
+	public float tumble;
+	private Rigidbody2D rigid;
+
+	void Start () {
+
+		rigid = GetComponent<Rigidbody2D>();
+		Quaternion randomRotation = Random.rotation;
+		float rotation = randomRotation.x;
+		rigid.AddTorque(rotation * tumble);
+	
+	}
+
+}
