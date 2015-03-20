ad04b85cefbfc28c67ee1a5b2db454a1d958babd
demonslayer <margaret.k.mcnulty@gmail.com>
2015-03-20 10:05:52 -0700

    ship can asplode too

--- SpaceJunk/Assets/scripts/DestroyOnCollision.cs
+++ SpaceJunk/Assets/scripts/DestroyOnCollision.cs
@@ -4,10 +4,15 @@ using System.Collections;
 public class DestroyOnCollision : MonoBehaviour {
 
 	public GameObject explosion;
+	public GameObject playerExplosion;
 
 	void OnTriggerEnter2D(Collider2D other) {
 
 		Instantiate(explosion, transform.position, transform.rotation);
+
+		if (other.tag == "Player") {
+			Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
+		}
 
 		Destroy(other.gameObject);
 		Destroy(gameObject);
